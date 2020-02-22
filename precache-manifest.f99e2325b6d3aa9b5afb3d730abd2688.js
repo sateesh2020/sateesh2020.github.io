548a4197a44e6db8eb6c4a76e2aeeafac72584ea
Satheesh Valluru <vallurusateesh2009@gmail.com>
2020-02-22 20:23:47 +0530

    Updates

---
+++ precache-manifest.f99e2325b6d3aa9b5afb3d730abd2688.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51fe8b7fd1ba2ce57b85d7fdd337470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104931c5c7a27b008eb6",
+    "url": "/static/css/main.0897b383.chunk.css"
+  },
+  {
+    "revision": "23d922eba719f28fcca3",
+    "url": "/static/js/2.b04d7e68.chunk.js"
+  },
+  {
+    "revision": "e2c3aa2cd13f0cc0d401299699f31cf1",
+    "url": "/static/js/2.b04d7e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104931c5c7a27b008eb6",
+    "url": "/static/js/main.620dee71.chunk.js"
+  },
+  {
+    "revision": "f711d2465371943f0de9",
+    "url": "/static/js/runtime-main.0646d9b0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6dacc7f0ebf758351fb02f3ff6cb325f",
+    "url": "/static/media/contactUs.6dacc7f0.svg"
+  },
+  {
+    "revision": "56387e7bf0fb0ae71ac6864509bcb1c3",
+    "url": "/static/media/developerActivity.56387e7b.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "8973e2b1eb470b11fe0cf461492c41d1",
+    "url": "/static/media/manOnTable.8973e2b1.svg"
+  },
+  {
+    "revision": "06a1ffa562f4050da335cce2488ad055",
+    "url": "/static/media/talksCardBack.06a1ffa5.svg"
+  }
+]);
